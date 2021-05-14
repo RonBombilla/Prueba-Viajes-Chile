9cd8b79718331d86fcf131972ec685859dde6611
RonBombilla <aaron.ubij@gmail.com>
2021-05-14 00:49:40 -0400

    prueba
    segundo commit de prueba

--- Prueba Viajes Chile/assets/js/main.js
+++ Prueba Viajes Chile/assets/js/main.js
@@ -13,33 +13,3 @@
         q.parentNode.insertBefore(js, q)
     }
 })();
-
-$(document).ready(function() {
-    var scroll_start = 0;
-    var startchange = $('#startchange');
-    var offset = startchange.offset();
-
-    $(document).scroll(function() {
-        scroll_start = $(this).scrollTop();
-        if (scroll_start > offset.top) {
-            $('#navbar').css('background-color', 'black');
-        } else {
-            $('#navbar').css('background-color', 'transparent');
-        }
-    });
-});
-
-$(document).ready(function() {
-    var scroll_start = 0;
-    var startchange = $('#startchange');
-    var offset = startchange.offset();
-
-    $(document).scroll(function() {
-        scroll_start = $(this).scrollTop();
-        if (scroll_start > offset.top) {
-            $('#navbar_main').css('background-color', 'black');
-        } else {
-            $('#navbar_main').css('background-color', 'transparent');
-        }
-    });
-});
