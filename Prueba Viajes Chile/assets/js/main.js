efe23688a09e933b0999500b4240cd8b36c81877
RonBombilla <aaron.ubij@gmail.com>
2021-05-14 01:03:18 -0400

    5° Commit
    Mod. final para js y css de efecto para navbar

--- Prueba Viajes Chile/assets/js/main.js
+++ Prueba Viajes Chile/assets/js/main.js
@@ -22,6 +22,21 @@ $(document).ready(function() {
     $(document).scroll(function() {
         scroll_start = $(this).scrollTop();
         if (scroll_start > offset.top) {
+            $('#navbar_main').css('background-color', 'black');
+        } else {
+            $('#navbar_main').css('background-color', 'transparent');
+        }
+    });
+});
+
+$(document).ready(function() {
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+
+    $(document).scroll(function() {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset.top) {
             $('#navbar').css('background-color', 'black');
         } else {
             $('#navbar').css('background-color', 'transparent');
