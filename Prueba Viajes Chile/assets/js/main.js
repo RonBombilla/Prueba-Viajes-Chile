bcb1e60c3b7c164f1b84924dd5e6afb6bec0c00d
RonBombilla <aaron.ubij@gmail.com>
2021-05-14 01:01:04 -0400

    4° Commit
    mod. js y ccs para efecto navbar

--- Prueba Viajes Chile/assets/js/main.js
+++ Prueba Viajes Chile/assets/js/main.js
@@ -13,3 +13,18 @@
         q.parentNode.insertBefore(js, q)
     }
 })();
+
+$(document).ready(function() {
+    var scroll_start = 0;
+    var startchange = $('#startchange');
+    var offset = startchange.offset();
+
+    $(document).scroll(function() {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset.top) {
+            $('#navbar').css('background-color', 'black');
+        } else {
+            $('#navbar').css('background-color', 'transparent');
+        }
+    });
+});
